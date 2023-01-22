e143df593805a85d3cb1dff6a45b8753d724022f
Julien Widmer <ju.widmer@pm.me>
2023-01-22 14:25:56 -0500

    Add solution for task a

--- index.js
+++ index.js
@@ -3,3 +3,30 @@
 //
 // Julien Widmer - 101320111
 
+/*
+a) Delete "canada.txt" and "usa.txt" if already exist using fs module
+ */
+const fs = require("fs");
+
+function deleteFile(name) {
+    try {
+        fs.unlinkSync(name);
+        console.log(`Task A --- "${name}" was DELETED.`)
+    } catch (error) {
+        console.log(`Task A --- "${name}" was NOT deleted:\n\t${error.message}`);
+    }
+}
+
+deleteFile("canada.txt");
+deleteFile("usa.txt");
+
+
+/*
+b) Filter data of Canada and write data to "canada.txt"
+ */
+
+
+
+/*
+c) Filter data of "United States" and write data to "usa.txt"
+ */
