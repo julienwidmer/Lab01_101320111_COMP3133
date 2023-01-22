2af89f8e2e7bae1837e8b50bb18f1e7cb0aa2781
Julien Widmer <ju.widmer@pm.me>
2023-01-22 17:24:40 -0500

    Refactor solutions

--- index.js
+++ index.js
@@ -3,22 +3,26 @@
 //
 // Julien Widmer - 101320111
 
+const fs = require("fs");
+
+const canadaFileName = "canada.txt";
+const usaFileName = "usa.txt";
+const countriesFileName = "input_countries.csv";
+
+
 /*
 a) Delete "canada.txt" and "usa.txt" if already exist using fs module
  */
-const fs = require("fs");
-const canadaFileName = "canada.txt";
-const usaFileName = "usa.txt";
-
 function deleteFile(name) {
     try {
         fs.unlinkSync(name);
-        console.log(`Task A --- "${name}" was DELETED.`);
+        console.log(`"${name}" was DELETED.`);
     } catch (error) {
-        console.log(`Task A --- "${name}" was NOT deleted:\n\t${error.message}`);
+        console.log(`"${name}" was NOT deleted:\n\t${error.message}`);
     }
 }
 
+console.log("--- Task A ---");
 deleteFile(canadaFileName);
 deleteFile(usaFileName);
 
@@ -26,49 +30,35 @@ deleteFile(usaFileName);
 /*
 b) Filter data of Canada and write data to "canada.txt"
  */
+function createFile(fileName, filter) {
+    try {
+        // --- Read file
+        const stringData = fs.readFileSync(countriesFileName, "utf-8");
+
+        // Convert string to array of rows using return line separator
+        let rows = stringData.split("\r\n");
+        // Retrieve headers
+        const headers = rows.shift();
+        // Filter rows
+        rows = rows.filter(row => row.toLowerCase().includes(filter.toLowerCase()));
+
+        // Merge headers and rows array to create result string
+        const filteredData = headers + "\r\n" + rows.join("\r\n");
+
+        // --- Write file
+        fs.writeFileSync(fileName, filteredData);
+        console.log(`"${fileName}" was CREATED.`);
+    } catch (error) {
+        console.log(`"${countriesFileName}" was NOT read:\n\t${error.message}`);
+    }
+}
+
+console.log("--- Task B ---");
+createFile(canadaFileName, "Canada");
+
+
 /*
 c) Filter data of "United States" and write data to "usa.txt"
  */
-const countriesFileName = "input_countries.csv";
-fs.open(countriesFileName, "r", (error, fd) => {
-    if (error) {
-        console.log(`Task B --- "${countriesFileName}" could NOT be opened:\n\t${error.message}`);
-        return;
-    }
-
-    // Read
-    fs.readFile(countriesFileName, "utf-8", (error, stringData) => {
-        if (error) {
-            console.log(`Task B --- "${countriesFileName}" could NOT be read:\n\t${error.message}`);
-            return;
-        }
-
-        // Convert string to array of rows
-        const rows = stringData.split("\r\n");
-
-        // Retrieve headers
-        const headers = rows.shift();
-
-        // Retrieve data for Canada
-        const canadianRows = rows.filter(row => row.toLowerCase().includes("canada"));
-
-        // Retrieve data for United States
-        const usaRows = rows.filter(row => row.toLowerCase().includes("united states"));
-
-        // Create string data from headers and rows
-        const canadianStringData = headers + "\r\n" + canadianRows.join("\r\n");
-        const usaStringData = headers + "\r\n" + usaRows.join("\r\n");
-
-        fs.writeFile(canadaFileName, canadianStringData, (error) => {
-            if (error) {
-                console.log(`Task B --- "${canadaFileName}" could NOT be written:\n\t${error.message}`);
-            }
-        })
-
-        fs.writeFile(usaFileName, usaStringData, (error) => {
-            if (error) {
-                console.log(`Task B --- "${usaFileName}" could NOT be written:\n\t${error.message}`);
-            }
-        })
-    });
-});
+console.log("--- Task C ---");
+createFile(usaFileName, "United States");
